1b932baee89f81d61912b720c5929bddb3114986
wavefox3921 <wavefox3921@gmail.com>
2025-06-22 20:03:15 +0900

    Update development log at 2025-06-22 20:03:14

--- src/fusion8bio8flash8brain6web_log.py
+++ src/fusion8bio8flash8brain6web_log.py
@@ -1392,3 +1392,27 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+# Update at 2025-06-22 20:03:14
+# Fixed some bugs
+# Refactored the code
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
