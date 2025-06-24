94963227ba22a942567c0f4f20bc7400f688e79c
wavefox3921 <wavefox3921@gmail.com>
2025-06-25 07:56:09 +0900

    Update development log at 2025-06-25 07:56:08

--- src/fusion8bio8flash8brain6web_log.py
+++ src/fusion8bio8flash8brain6web_log.py
@@ -1416,3 +1416,16 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
+
+# Update at 2025-06-25 07:56:08
+# Added unit tests
+
+import json
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_from_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
