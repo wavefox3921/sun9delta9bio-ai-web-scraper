387f605d510685923a47905c4afac2e8fcdd5997
wavefox3921 <wavefox3921@gmail.com>
2025-11-22 05:51:12 +0900

    Update development log at 2025-11-22 05:51:10

--- src/fusion8bio8flash8brain6web_log.py
+++ src/fusion8bio8flash8brain6web_log.py
@@ -1778,3 +1778,26 @@ def count_words(text):
     for word in words:
         word_count[word] = word_count.get(word, 0) + 1
     return word_count
+
+# Update at 2025-11-22 05:51:10
+# Added unit tests
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
